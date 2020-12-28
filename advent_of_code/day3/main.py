a584bf82bc5914de70cdab2f5edb16d922382f63
Miles Bardon <milesbardon@gmail.com>
2020-12-28 11:47:46 +0000

    Optimising Day 3

--- advent_of_code/day3/main.py
+++ advent_of_code/day3/main.py
@@ -11,23 +11,23 @@ def find_covered_points(wire):
         if direction == 'U':
             for i in range(distance):
                 wire_points.append(
-                    {'coord': (x, y + i), 'completion': i})
+                    ((x, y + i), i))
         elif direction == 'R':
             for i in range(distance):
                 wire_points.append(
-                    {'coord': (x + i, y), 'completion': i})
+                    ((x + i, y), i))
         elif direction == 'L':
             for i in range(distance):
                 wire_points.append(
-                    {'coord': (x - i, y), 'completion': i})
+                    ((x - i, y), i))
         else:
             for i in range(distance):
                 wire_points.append(
-                    {'coord': (x, y - i), 'completion': i})
+                    ((x, y - i), i))
 
         # Get the final X,Y coordinates from this command.
-        x = wire_points[len(wire_points) - 1]['coord'][0]
-        y = wire_points[len(wire_points) - 1]['coord'][1]
+        x = wire_points[len(wire_points) - 1][0][0]
+        y = wire_points[len(wire_points) - 1][0][1]
 
     return wire_points
 
@@ -35,17 +35,15 @@ def find_covered_points(wire):
 def wire_length_to_intercept(wire1, wire2, intercept):
     # As intercepts doesn't contain the rest of the dictionary, we
     # go to find it by searching for the coordinate in the original list
-    w1_index = next(i for i, item in enumerate(
-        wire1) if item['coord'] == intercept)
-    w2_index = next(i for i, item in enumerate(
-        wire2) if item['coord'] == intercept)
+    w1_index = next(i for i, item in enumerate(wire1) if item[0] == intercept)
+    w2_index = next(i for i, item in enumerate(wire2) if item[0] == intercept)
 
     # The result is the combined length of each array of steps
     # where progress has been made.
     wire1_length = len(
-        list(filter(lambda d: d['completion'] != 0, wire1[:w1_index + 1])))
+        list(filter(lambda d: d[1] != 0, wire1[:w1_index + 1])))
     wire2_length = len(
-        list(filter(lambda d: d['completion'] != 0, wire2[:w2_index + 1])))
+        list(filter(lambda d: d[1] != 0, wire2[:w2_index + 1])))
 
     return wire1_length + wire2_length
 
@@ -55,11 +53,10 @@ def part_one(input):
     wire1 = find_covered_points(input[0].split(','))
     wire2 = find_covered_points(input[1].split(','))
 
-    intercepts = set([w['coord'] for w in wire1]) & set(
-        [w['coord'] for w in wire2])
+    intercepts = set([w[0] for w in wire1]) & set(
+        [w[0] for w in wire2])
 
-    return min(filter(bool, map(lambda intercept: abs(
-        intercept[0]) + abs(intercept[1]), intercepts)))
+    return min(filter(bool, map(lambda intercept: abs(intercept[0]) + abs(intercept[1]), intercepts)))
 
 
 def part_two(input):
@@ -67,7 +64,6 @@ def part_two(input):
     wire1 = find_covered_points(input[0].split(','))
     wire2 = find_covered_points(input[1].split(','))
 
-    intercepts = set([w['coord'] for w in wire1]) & set(
-        [w['coord'] for w in wire2])
+    intercepts = set([w[0] for w in wire1]) & set([w[0] for w in wire2])
 
     return min(filter(bool, map(lambda intercept: wire_length_to_intercept(wire1, wire2, intercept), intercepts)))
